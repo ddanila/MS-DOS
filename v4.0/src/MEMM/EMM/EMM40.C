da907a40db1994d06016af85c641b2a0d249ac5e
Danila Sukharev <dan.sukharev@gmail.com>
2026-08-27 19:47:58 +0300

    Make EMM386 map-array stepping explicit

--- v4.0/src/MEMM/EMM/EMM40.C
+++ v4.0/src/MEMM/EMM/EMM40.C
@@ -232,7 +232,7 @@ UndefinedFunction()
  */
 GetMappablePAddrArray() 
 {
-	struct mappable_page far *u_ptr;
+	unsigned char far *u_ptr;
 	int	n_pages;
 	int	i;
 	struct mappable_page *mp = mappable_pages;
@@ -241,12 +241,15 @@ GetMappablePAddrArray()
 
 	if ( regp->hregs.h.ral == 0 ) {
 		if ( n_pages > 0 ) {
-			u_ptr = (struct mappable_page far *)dest_addr(); /* ES:DI */
-			for (i=0 ; i < 48 ; i++)
-				if ((unsigned char)EMM_MPindex[i] != 0xff)
-					copyout(u_ptr++,
+			u_ptr = (unsigned char far *)dest_addr(); /* ES:DI */
+			for (i=0 ; i < 48 ; i++) {
+				if ((unsigned char)EMM_MPindex[i] != 0xff) {
+					copyout(u_ptr,
 						mp + EMM_MPindex[i],
 						sizeof(struct mappable_page) );
+					u_ptr += sizeof(struct mappable_page);
+				}
+			}
 		}
 	} else if ( regp->hregs.h.ral != 1 ) {
 		setAH(INVALID_SUBFUNCTION);
